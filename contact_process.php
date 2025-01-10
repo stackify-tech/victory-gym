e88f5e48ceafda47bf8da07e194b5cc101e1f6a7
vaishnavi0175 <vaishnavis0715@gmail.com>
2025-01-10 22:34:11 +0530

    text updated

--- contact_process.php
+++ contact_process.php
@@ -1,37 +1,55 @@
 <?php
+    // Set the recipient email
+    $to = "chippuu715@gmail.com";
 
-    $to = "rockybd1995@gmail.com";
-    $from = $_REQUEST['email'];
-    $name = $_REQUEST['name'];
-    $subject = $_REQUEST['subject'];
-    $number = $_REQUEST['number'];
-    $cmessage = $_REQUEST['message'];
+    // Sanitize and validate inputs
+    $from = filter_var($_POST['email'], FILTER_SANITIZE_EMAIL);
+    $name = htmlspecialchars($_POST['name']);
+    $number = htmlspecialchars($_POST['number']);
+    $cmessage = htmlspecialchars($_POST['message']);
 
-    $headers = "From: $from";
-	$headers = "From: " . $from . "\r\n";
-	$headers .= "Reply-To: ". $from . "\r\n";
-	$headers .= "MIME-Version: 1.0\r\n";
-	$headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
+    // Validate email
+    if (!filter_var($from, FILTER_VALIDATE_EMAIL)) {
+        die("Invalid email format");
+    }
 
-    $subject = "You have a message from your Bitmap Photography.";
+    // Validate phone number
+    if (!preg_match('/^\d{10}$/', $number)) {
+        die("Invalid phone number format");
+    }
 
-    $logo = 'img/logo.png';
+    // Prepare email headers
+    $headers = "From: $from\r\n";
+    $headers .= "Reply-To: $from\r\n";
+    $headers .= "MIME-Version: 1.0\r\n";
+    $headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
+
+    // Email subject
+    $email_subject = "You have a message from your website";
+
+    // Email body
+    $logo = 'img/logo.png';  // Ensure this path is correct
     $link = '#';
 
-	$body = "<!DOCTYPE html><html lang='en'><head><meta charset='UTF-8'><title>Express Mail</title></head><body>";
-	$body .= "<table style='width: 100%;'>";
-	$body .= "<thead style='text-align: center;'><tr><td style='border:none;' colspan='2'>";
-	$body .= "<a href='{$link}'><img src='{$logo}' alt=''></a><br><br>";
-	$body .= "</td></tr></thead><tbody><tr>";
-	$body .= "<td style='border:none;'><strong>Name:</strong> {$name}</td>";
-	$body .= "<td style='border:none;'><strong>Email:</strong> {$from}</td>";
-	$body .= "</tr>";
-	$body .= "<tr><td style='border:none;'><strong>Subject:</strong> {$csubject}</td></tr>";
-	$body .= "<tr><td></td></tr>";
-	$body .= "<tr><td colspan='2' style='border:none;'>{$cmessage}</td></tr>";
-	$body .= "</tbody></table>";
-	$body .= "</body></html>";
+    $body = "<!DOCTYPE html><html lang='en'><head><meta charset='UTF-8'><title>Express Mail</title></head><body>";
+    $body .= "<table style='width: 100%;'>";
+    $body .= "<thead style='text-align: center;'><tr><td style='border:none;' colspan='2'>";
+    $body .= "<a href='{$link}'><img src='{$logo}' alt='Logo'></a><br><br>";
+    $body .= "</td></tr></thead><tbody><tr>";
+    $body .= "<td style='border:none;'><strong>Name:</strong> {$name}</td>";
+    $body .= "<td style='border:none;'><strong>Email:</strong> {$from}</td>";
+    $body .= "</tr>";
+    $body .= "<tr><td style='border:none;'><strong>Phone Number:</strong> {$number}</td></tr>";
+    $body .= "<tr><td colspan='2' style='border:none;'><strong>Message:</strong><br>{$cmessage}</td></tr>";
+    $body .= "</tbody></table>";
+    $body .= "</body></html>";
 
-    $send = mail($to, $subject, $body, $headers);
-
+    // Send email
+    if (mail($to, $email_subject, $body, $headers)) {
+        // Redirect to the thank you page after the email is successfully sent
+        header("Location: thank-you.html");
+        exit();
+    } else {
+        echo "Failed to send the message. Please try again.";
+    }
 ?>
